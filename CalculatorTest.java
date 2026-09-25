8cb3ed5e85d1b2015f0ae9fbde17e524c6943067
cje361 <edwards.christian494@student.ccm.edu>
2026-09-25 14:07:31 -0400

    completed lab5

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,3 +1,10 @@
 public class CalculatorTest {
-    
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println("4 + 7 = " + calculator.add(4,7));
+        System.out.println("2.5 + 3.75 = " + calculator.add(2.5,3.75));
+        System.out.println("1 + 2 + 3 = " + calculator.add(1,2,3));
+        System.out.println(calculator.add("CMP129", "EH210"));
+    }
 }
